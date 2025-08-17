b4c569ca169223befbfafb5acf8cf38af0ba6f9a
BonusDukks <alexander.ung562@gmail.com>
2025-08-17 14:15:43 +0930

    added .DS_Store to .gitignore and implemented i/o for calculator client

--- CalculatorClient.java
+++ CalculatorClient.java
@@ -3,27 +3,85 @@ import java.rmi.RemoteException;
 import java.rmi.NotBoundException;
 import java.net.MalformedURLException;
 import java.util.Stack;
+import java.util.Scanner;
 
 public class CalculatorClient {
   public static void main(String[] args) {
     try {
       // Look up the remote object from the RMI registry
-      Calculator goal = (Calculator) Naming.lookup("rmi://localhost:1099/goal");
+      Calculator calculator = (Calculator) Naming.lookup("rmi://localhost:1099/Calculator");
 
-      // Test the sum method with sample values
-      int result = goal.sum(10, 20);
-      System.out.println("10 + 20 = " + result);
+      boolean exit = false;
 
-      // Test the push and get method
-      goal.push(result);
-      goal.push(5);
-      Stack<Integer> serverStack = goal.getStack();
-      System.out.println(serverStack);
+      while (!exit) {
+        Scanner scannerObj = new Scanner(System.in);
+        System.out.println("""
 
-      // Test the pop method
-      goal.pop();
-      serverStack = goal.getStack();
-      System.out.println(serverStack);
+            Calculator Operators
+            (1) - Push Value
+            (2) - Pop
+            (3) - Check if stack is empty
+            (4) - Minimum of Stack
+            (5) - Maximum of Stack
+            (6) - LCM (Lowest Common Multiple) of Stack
+            (7) - GCD (Greatest Common Divisor) of Stack
+            (8) - Delay Pop
+            (9) - Exit
+            """);
+
+        int userInput = scannerObj.nextInt();
+
+        switch (userInput) {
+          case 1:
+            System.out.println("\nInput an Integer to be pushed to the stack:");
+            int pushedValue = scannerObj.nextInt();
+            calculator.pushValue(pushedValue);
+            System.out.println("Current stack is: " + calculator.getStack());
+            break;
+          case 2:
+            int poppedValue = calculator.pop();
+            System.out.println("\nInteger popped from the stack was: " + poppedValue);
+            System.out.println("Current stack is: " + calculator.getStack());
+            break;
+          case 3:
+            if (calculator.isEmpty()) {
+              System.out.println("Stack is empty.");
+            } else {
+              System.out.println("Stack is not empty.");
+            }
+            break;
+          case 4:
+            calculator.pushOperation("min");
+            System.out.println("Current stack after min operation is: " + calculator.getStack());
+            break;
+          case 5:
+            calculator.pushOperation("max");
+            System.out.println("Current stack after max operation is: " + calculator.getStack());
+            break;
+          case 6:
+            calculator.pushOperation("lcm");
+            System.out.println("Current stack after lcm operation is: " + calculator.getStack());
+            break;
+          case 7:
+            calculator.pushOperation("gcd");
+            System.out.println("Current stack after gcd operation is: " + calculator.getStack());
+            break;
+          case 8:
+            System.out.println("Enter delay value (in milliseconds): ");
+            int delayValue = scannerObj.nextInt();
+            calculator.pushValue(delayValue);
+            System.out.println("Current stack is: " + calculator.getStack());
+            break;
+
+          case 9:
+            exit = true;
+
+            scannerObj.close();
+            break;
+
+        }
+
+      }
 
     } catch (NotBoundException e) {
       System.err.println("Calculator service not found: " + e.getMessage());
